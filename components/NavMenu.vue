c141f115e7bd7c3420c00ec26606d5655fe575fb
alexlyaps <a34270622@gmail.com>
2025-06-11 15:07:50 +0200

    fix: close on click outside

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-end items-end gap-8">
+  <div class="flex flex-col justify-end items-end gap-8" ref="burgerEl">
     <Burger :is-opened="isOpened" @click="toggleMenu" />
 
     <div
@@ -8,7 +8,7 @@
         isOpened ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0',
       ]"
     >
-      <NavItem v-for="link of links" :path="link.path">{{
+      <NavItem v-for="link of links" :path="link.path" @click="toggleMenu">{{
         link.label
       }}</NavItem>
     </div>
@@ -23,4 +23,22 @@ const isOpened = ref(false);
 const toggleMenu: () => void = () => {
   isOpened.value = !isOpened.value;
 };
+
+const burgerEl = ref<HTMLElement | null>(null);
+const handleOutsudeClick = (e: MouseEvent) => {
+  if (burgerEl.value && !burgerEl.value.contains(e.target as Node)) {
+    isOpened.value = false;
+  }
+};
+
+onMounted(() => {
+  if (burgerEl.value) {
+    console.log(burgerEl.value);
+  }
+  document.addEventListener("click", handleOutsudeClick);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("click", handleOutsideClick);
+});
 </script>
--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,10 +1,13 @@
 <template>
   <header class="flex items-start justify-between">
-    <h1
-      class="serif-regular h-8 w-2 text-2xl md:text-3xl whitespace-nowrap overflow-visible"
-    >
-      alevtina lyapunova
-    </h1>
+    <NuxtLink to="/">
+      <h1
+        class="serif-regular h-8 w-2 text-2xl md:text-3xl whitespace-nowrap overflow-visible"
+      >
+        alevtina lyapunova
+      </h1>
+    </NuxtLink>
+
     <NavMenu />
   </header>
 </template>
