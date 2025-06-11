e26db71fc6d953536b53628b4b62f40dcdb6c64a
alexlyaps <a34270622@gmail.com>
2025-06-11 15:50:18 +0200

    fix: missing pages/pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,27 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   ssr: true,
-  nitro: {
-    prerender: {
-      ignore: [
-        "/about",
-        "/present-continues",
-        "/a-few-stories",
-        "/boccaccio-dream",
-        "/play-with-a-snake",
-        "/beside-the-color",
-        "/rabbits",
-        "/learning-from-bowerbird",
-        "/life-drawings",
-        "/family",
-        "/movement-score",
-        "/you-look-like-an-angel",
-        "/baltic-sand",
-
-        "/contacts",
-      ],
-    },
-  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/fonts", "@nuxt/image"],
